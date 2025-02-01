04db7807acc96733c6f867624dfa180ace3cd384
yaorui <56313449@qq.com>
2025-02-01 19:58:27 +0800

    init

--- func.py
+++ func.py
@@ -204,6 +204,10 @@ def myFunc(rknn_lite, IMG):
     IMG = cv2.resize(IMG, (IMG_SIZE, IMG_SIZE))
     outputs = rknn_lite.inference(inputs=[IMG])
 
+    print(" --------------------------------- ")
+    print(" outputs: ", outputs)
+    print(" --------------------------------- ")
+
     input0_data = outputs[0].reshape([3, -1]+list(outputs[0].shape[-2:]))
     input1_data = outputs[1].reshape([3, -1]+list(outputs[1].shape[-2:]))
     input2_data = outputs[2].reshape([3, -1]+list(outputs[2].shape[-2:]))
