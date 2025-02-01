03d8e81eb22da287e0bea433a43ea529d652464c
yaorui <56313449@qq.com>
2025-02-01 23:16:55 +0800

    init

--- rknnpool.py
+++ rknnpool.py
@@ -6,17 +6,18 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 def initRKNN(rknnModel="./rknnModel/yolov5s.rknn", id=0):
     rknn_lite = RKNNLite()
     ret = rknn_lite.load_rknn(rknnModel)
+    print("Load RKNN rknnModel: ", id)
     if ret != 0:
         print("Load RKNN rknnModel failed")
         exit(ret)
     if id == 0:
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0)
+        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0, target="rk3588", perf_mode="normal")
     elif id == 1:
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_1)
+        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_1, target="rk3588", perf_mode="normal")
     elif id == 2:
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_2)
+        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_2, target="rk3588", perf_mode="normal")
     elif id == -1:
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0_1_2)
+        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0_1_2, target="rk3588", perf_mode="normal")
     else:
         ret = rknn_lite.init_runtime()
     if ret != 0:
