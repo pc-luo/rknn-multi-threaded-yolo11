971a8fd7d15cab70aabfb9a8e04387519416c139
yaorui <56313449@qq.com>
2025-02-01 23:37:13 +0800

    init

--- func.py
+++ func.py
@@ -202,7 +202,6 @@ def myFunc(rknn_lite, IMG):
     # IMG = letterbox(IMG)
     # 强制放缩
     IMG = cv2.resize(IMG, (IMG_SIZE, IMG_SIZE))
-    IMG = IMG.reshape((1, 224, 224, 3))
 
     try:
         outputs = rknn_lite.inference(inputs=[IMG])
